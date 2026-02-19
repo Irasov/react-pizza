486d300990b1e2427e4ed383404cd8431b7afdb7
Irasov <srnan@yandex.ru>
2026-02-19 12:14:13 +0500

    (refactor, add): #8 change style css  and add layout cart

--- react-pizza/src/pages/Home.jsx
+++ react-pizza/src/pages/Home.jsx
@@ -16,9 +16,10 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
         setItems(arr);
         setIsLoading(false);
       });
+      window.scrollTo(0, 0);
   }, []);
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -29,7 +30,7 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
           ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
           : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
-    </>
+    </div>
   )
 }
 
