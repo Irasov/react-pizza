40d4609e1ba2039e1087244e088d39225778af93
Irasov <srnan@yandex.ru>
2026-02-28 13:57:26 +0500

    refactor: change folder name

--- react-pizza/src/index.js
+++ react-pizza/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './redax/store';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
