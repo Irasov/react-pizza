572690fac4021eb871d6485d4acac6600b1ea743
Irasov <srnan@yandex.ru>
2026-03-16 16:44:13 +0500

    fix: calculate totalPrice

--- react-pizza/src/redux/cart/slice.ts
+++ react-pizza/src/redux/cart/slice.ts
@@ -19,20 +19,18 @@ export const cartSlice = createSlice({
           count: 1,
         });
       }
-
       state.totalPrice = calcTotalPrice(state.items);
     },
     minusItem: (state, action: PayloadAction<string>) => {
       const findItem = state.items.find((obj) => obj.id === action.payload);
       if (findItem && findItem.count > 1) {
         findItem.count--;
+        state.totalPrice = calcTotalPrice(state.items);
       }
     },
     removeItem: (state, action: PayloadAction<string>) => {
       state.items = state.items.filter((obj) => obj.id !== action.payload);
-      state.totalPrice = state.items.reduce((sum, obj) => {
-        return obj.price + sum;
-      }, 0);
+      state.totalPrice = calcTotalPrice(state.items);
     },
     clearItems: (state) => {
       state.items = [];
