c5f9f6178c07505f9076c28035a5af644a584fe3
Irasov <srnan@yandex.ru>
2026-03-17 19:43:21 +0500

    feat: add settings for deploy

--- react-pizza/src/index.tsx
+++ react-pizza/src/index.tsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/react-pizza/'>
     <Provider store={store}>
       <App />
     </Provider>
